20fc2766b387e836995e214c97dc9fca4072729c
VnkHO <ho.vincent.nk@gmail.com>
2020-01-07 16:50:02 +0100

    Counter app in React Hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import Button from './components/Button'
+
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const incrementCount = increment => {
+    setCount(count + increment)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter app</h1>
+      <p>{count}</p>
+      <Button increment={1} onClickFunction={incrementCount} />
+      <Button increment={10} onClickFunction={incrementCount} />
+      <Button increment={100} onClickFunction={incrementCount} />
+      <Button increment={1000} onClickFunction={incrementCount} />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({ increment, onClickFunction }) => {
+
+  const handleClick = () => {
+    onClickFunction(increment)
+  }
+
+  return <button onClick={handleClick}>+{increment}</button>
+}
+
+export default Button
